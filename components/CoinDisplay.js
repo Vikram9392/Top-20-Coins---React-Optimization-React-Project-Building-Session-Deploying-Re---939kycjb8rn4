76c86fa8d4e1282096d19bf8ca98db47571ad71c
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 15:25:04 +0530

    Update CoinDisplay.js

--- components/CoinDisplay.js
+++ components/CoinDisplay.js
@@ -1,40 +1,21 @@
-import { useState, useEffect} from "react";
-import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
+import CoinCard from "./CoinCard";
 
-function CoinDetail() {
-  const [coin, setCoin] = useState([]);
-  const[loader, setLoader] = useState(true);
-  const router = useRouter();
-  const {coin_id} = router.query;
-//  console.log("router.query ,",router.query);
-  console.log("inside details page",coin_id);
+function CoinDisplay(props) {
+  const coindata = props.coin;
 
-  async function getCoinDetails()
-  {
-    const data = await fetch(`https://api.coinlore.net/api/ticker/?id=${coin_id}`);
-    const res = await data.json();
-    setCoin(res[0]);
-  }
-  useEffect(()=>{
-    getCoinDetails();
-    setLoader(false);
-  },[]);
   return (
-    loader==true?(<p>Loading...</p>):(<div className="coin-detail">
-    <div className="name-symbol">
-      <h1 className="name">{coin.name} </h1>
-      <h2 className="symbol">{coin.symbol}</h2>
+    <div className="home">
+      <h1>Top 20 Cryptos</h1>
+      <div className="coins-container">
+        {coindata.length>0 && coindata.slice(0,20).map((coin,i) => {
+          
+          return(<CoinCard coin={coin}/>)
+        }
+        )}
+      </div>
     </div>
-    <div className="market-description">
-      <p className="coin-rank">Rank: {coin.rank}</p>
-      <p className="coin-price">Price: ${coin.price_usd}</p>
-      <p className="coin-market-cap">Market Cap: ${coin.market_cap_usd}</p>
-      <p className="coin-total-supply">Total Supply: {coin.tsupply}</p>
-      <p className="coin-max-supply">Max Supply: {coin.msupply==""?"N/A":(coin.msupply)}</p>
-    </div>
-  </div>)
-    
   );
 }
 
-export default CoinDetail;
+export default CoinDisplay;
