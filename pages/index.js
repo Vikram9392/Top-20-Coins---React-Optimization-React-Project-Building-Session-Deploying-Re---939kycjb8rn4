18b5608ce124c3769b2adb5c3e1cc5730201723e
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 15:19:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import CoinDisplay from "../components/CoinDisplay";
-
+import React, { useState, useEffect } from "react";
 export default function Home() {
-  return <CoinDisplay />;
+  const [coins, setCoins] = useState([]);
+  async function getcoins(){
+    
+    let data = await fetch("https://api.coinlore.net/api/tickers/");
+    let res = await data.json();
+    setCoins(res.data);
+  }
+  useEffect(()=>{
+    getcoins();
+  },[]);
+  return <CoinDisplay coin={coins}/>;
 }
