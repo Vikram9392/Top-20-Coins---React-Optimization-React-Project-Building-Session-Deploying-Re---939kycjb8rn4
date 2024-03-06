1c1fa27ede4ed9c9040c4fafee1fe648096a465d
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 15:18:55 +0530

    Update CoinDisplay.js

--- components/CoinDisplay.js
+++ components/CoinDisplay.js
@@ -1,19 +1,40 @@
-import React from "react";
-import Link from "next/link";
+import { useState, useEffect} from "react";
+import { useRouter } from "next/router";
 
-function CoinCard(props) {
-  const data = props.coin;
+function CoinDetail() {
+  const [coin, setCoin] = useState([]);
+  const[loader, setLoader] = useState(true);
+  const router = useRouter();
+  const {coin_id} = router.query;
+//  console.log("router.query ,",router.query);
+  console.log("inside details page",coin_id);
+
+  async function getCoinDetails()
+  {
+    const data = await fetch(`https://api.coinlore.net/api/ticker/?id=${coin_id}`);
+    const res = await data.json();
+    setCoin(res[0]);
+  }
+  useEffect(()=>{
+    getCoinDetails();
+    setLoader(false);
+  },[]);
   return (
-    <div className="coin-card">
-      <h3 className="coin-name">{data.name}</h3>
-      <h4 className="coin-symbol">{data.symbol}</h4>
-      <p className="coin-rank">Rank: {data.rank}</p>
-      <p className="coin-price">Price: ${data.price_usd}</p>
-      <Link className="view-details-link" href={`/coins/${data.id}`}>
-        View Details
-      </Link>
+    loader==true?(<p>Loading...</p>):(<div className="coin-detail">
+    <div className="name-symbol">
+      <h1 className="name">{coin.name} </h1>
+      <h2 className="symbol">{coin.symbol}</h2>
     </div>
+    <div className="market-description">
+      <p className="coin-rank">Rank: {coin.rank}</p>
+      <p className="coin-price">Price: ${coin.price_usd}</p>
+      <p className="coin-market-cap">Market Cap: ${coin.market_cap_usd}</p>
+      <p className="coin-total-supply">Total Supply: {coin.tsupply}</p>
+      <p className="coin-max-supply">Max Supply: {coin.msupply==""?"N/A":(coin.msupply)}</p>
+    </div>
+  </div>)
+    
   );
 }
 
-export default CoinCard;
+export default CoinDetail;
